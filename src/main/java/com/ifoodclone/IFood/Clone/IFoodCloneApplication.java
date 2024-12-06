6ed5a6ce0ff4e1723b092284479b360fb4610b11
FillipeBonanome <incarnialol2@gmail.com>
2024-12-06 11:30:30 -0300

    First Commit
    Hello World!

---
+++ src/main/java/com/ifoodclone/IFood/Clone/IFoodCloneApplication.java
@@ -0,0 +1,13 @@
+package com.ifoodclone.IFood.Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFoodCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IFoodCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifoodclone/IFood/Clone/IFoodCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifoodclone.IFood.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IFoodCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
